5cbedb3993b1bf05ef05e68edde8086eae0136e6
gayemce <gayemce4@gmail.com>
2025-04-21 12:15:26 +0300

    feat(order-service): add initial setup with gin HTTP server

---
+++ order-service/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	gin.SetMode(gin.ReleaseMode)
+	app := gin.Default()
+
+	app.Use(gin.Recovery())
+
+	app.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "service is up and runnig",
+		})
+	})
+
+	srv := &http.Server{
+		Addr: fmt.Sprintf(":%d", 8001),
+		Handler: app,
+		ReadTimeout: 10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout: 30 * time.Second,
+		MaxHeaderBytes: 1 << 20, // 1MB
+	}
+
+	if err:= srv.ListenAndServe(); err != nil {
+		panic(err)
+	}
+}
